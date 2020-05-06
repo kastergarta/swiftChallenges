0ea69370e46cd333c775d3522488b97da6cb7cfd
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-05-06 00:48:35 -0400

    most common name - dictionary creatiom/traversal

---
+++ goodVsEvil.swift
@@ -0,0 +1,29 @@
+func mostCommonName(array: [String]) -> String {
+
+    var dictionary = [String: Int]()
+
+    for name in array {
+        if let count = dictionary[name] {
+            dictionary[name] = count + 1
+        } else {
+            dictionary[name] = 1
+        }
+    }
+
+    var mostCommonName = ""
+
+    for key in dictionary.keys {
+        if mostCommonName == "" {
+            mostCommonName = key
+        } else {
+            let count = dictionary[key]
+            if count > dictionary[mostCommonName] {
+                mostCommonName = key
+            }
+        }
+    }
+    return mostCommonName
+}
+
+let nameArr = ["Dennis", "Anna", "Kissinger", "Anna"]
+mostCommonName(nameArr)
