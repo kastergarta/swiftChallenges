c1bb1bcb5555a18628f5663bb42c6c52f4fae588
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-07-13 11:13:33 -0400

    flip game

---
+++ flipGame.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class FlipGame {
+    func generatePossibleNextMoves(_ s: String) -> [String] {
+        var res = [String]()
+        let sChars = Array(s)
+        
+        guard sChars.count > 1 else {
+            return res
+        }
+        
+        for i in 0..<sChars.count - 1 {
+            if sChars[i] == "+" && sChars[i + 1] == "+" {
+                res.append(String(sChars[0..<i] + "--" + sChars[i + 2..<sChars.count]))
+            }
+        }
+        
+        return res
+    }
+}
