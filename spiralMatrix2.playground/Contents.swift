9bc85307347aaccb7331a5904321b1fa2d7fae71
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-08-07 14:52:56 -0400

    spiral matrix 2 + need to fix

---
+++ spiralMatrix2.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class SpiralMatrixII {
+    func generateMatrix(_ n: Int) -> [[Int]] {
+        guard n > 0 else {
+            return [[Int]]()
+        }
+    
+        var num = 1
+        var res = Array(repeating: Array(repeating: 0, count: n), count: n)
+        
+        for layer in 0..<n / 2 {
+            let start = layer
+            let end = n - layer - 1
+            
+            // top
+            for i in start..<end {
+                res[start][i] = num
+                num += 1
+            }
+            
+            // right
+            for i in start..<end {
+                res[i][end] = num
+                num += 1
+            }
+            
+            // bottom
+            for i in stride(from: end, to: start, by: -1) {
+                res[end][i] = num
+                num += 1
+            }
+            
+            // left
+            for i in stride(from: end, to: start, by: -1) {
+                res[i][start] = num
+                num += 1
+            }
+        }
+        
+        // handle the center one
+        if n % 2 != 0 {
+            res[n / 2][n / 2] = n * n
+        }
+        
+        return res
+    }
+}
